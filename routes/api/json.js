a90bb8b93dd74bc8ea2de45b459eb52a8bba8877
Peter Knipp <peteraknipp@gmail.com>
2024-07-22 07:08:01 -0400

    Calculate logistics map values.

--- routes/api/json.js
+++ routes/api/json.js
@@ -1,5 +1,30 @@
 const router = require('express').Router();
-router.get('/', (req, res) => {
-  res.json({message: "Hello world"});
+
+const map = (r, xInitial, nMax) => {
+  let x = xInitial;
+  let n = 0;
+  let xs = [x];
+  while (n < nMax) {
+    n++;
+    x = r * x * (1 - x);
+    xs.push(x);
+  }
+  return xs;
+}
+
+router.get('', (req, res) => {
+  res.json({message: "Instructions go here."});
+});
+
+'/:id'
+router.get('/json/:rNmaxNmin', (req, res) => {
+  let rNmaxNmin = rNmaxNmin.split("_");
+  let [r, nMax, nMin] = rNmaxNmin.length === 3 ? rNmaxNmin : [...rNmaxNmin, 0];
+  ls xs = map(r, 0.5, nMax).slice(nMin);
+  res.json({message: JSON.stringify(xs)});
+});
+
+router.get('/html', (req, res) => {
+  res.json({message: "html output goes here."});
 });
 module.exports = router;
