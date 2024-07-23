2871b331cd2127dd8625cd65968c5dba6cca0e95
Peter Knipp <peteraknipp@gmail.com>
2024-07-22 23:34:49 -0400

    Change "/" to "" for slash route.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-router.get("/", (req, res) => {
+router.get("", (req, res) => {
     res.json({message: "Instructions go here."});
   });
 ["json", "svg"].forEach(route => router.use(`/${route}`, require(`./${route}`)));
