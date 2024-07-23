246325b8f63f721c566601d447016e5e9d5f9d67
Peter Knipp <peteraknipp@gmail.com>
2024-07-23 16:58:16 -0400

    Change from n-1 to n.

--- routes/api/svg.js
+++ routes/api/svg.js
@@ -15,7 +15,7 @@ router.get('/:rNmaxNmin', (req, res) => {
     svg.el = `${svg.el}<g transform="translate(${svg.padding.x}, ${svg.padding.y})">`;
     svg.el = `${svg.el}<rect height=${rect.size.y} width=${rect.size.x} fill="transparent" stroke="black" />`;
     let n = ys.length;
-    let r = rect.size.x / (n - 1) / 2 * 2 ** (rFactor < 3 ? 0 : rFactor < 3.44949 ? 1 : rFactor < 3.54409 ? 2 : 3);
+    let r = rect.size.x / n / 2 * 2 ** (rFactor < 3 ? 0 : rFactor < 3.44949 ? 1 : rFactor < 3.54409 ? 2 : 3);
     console.log("r = ", r);
     rect.padding = r;
     ys.forEach((y, i) => {
