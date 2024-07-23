baa07dd3977ca7a6b58f2440566edc616812f560
Peter Knipp <peteraknipp@gmail.com>
2024-07-23 13:06:43 -0400

    Is a space needed before /> of rect element?

--- routes/api/svg.js
+++ routes/api/svg.js
@@ -12,7 +12,7 @@ router.get('/:rNmaxNmin', (req, res) => {
     let rect = {size: {x: svg.size.x - 2 * svg.padding.x, y: svg.size.y - 2 * svg.padding.y}};
     svg.el = `<svg height=${svg.size.y} width=${svg.size.x}>`;
     svg.el = `${svg.el}<g transform="translate(${svg.padding.x}, ${svg.padding.y})">`;
-    svg.el = `${svg.el}<rect height=${rect.size.y} width=${rect.size.x}/>`;
+    svg.el = `${svg.el}<rect height=${rect.size.y} width=${rect.size.x} />`;
     svg.el = `${svg.el}</g></svg>`;
     // let dot = '<circle cx="800" cy="450" r="800"/>';
     res.send(svg.el);
