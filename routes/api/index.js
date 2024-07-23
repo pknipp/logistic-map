9a8e14f2cee93580a390143a4be5b33506de06d7
Peter Knipp <peteraknipp@gmail.com>
2024-07-23 19:54:21 -0400

    Bring slash route up one level (out of api directory).

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 router.get("", (req, res) => {
-    res.json({message: "Instructions go here."});
-  });
+  res.json({message: "Instructions go here."});
+});
 ["json", "svg"].forEach(route => router.use(`/${route}`, require(`./${route}`)));
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
-router.use('/api', require('./api'));
+// router.use('/api', require('./api'));
+router.get("", (req, res) => {
+    res.json({message: "Instructions go here."});
+});
 module.exports = router;
