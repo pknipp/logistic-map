0863cc87c8012f3c76738c38b0a04d156c79c826
Peter Knipp <peteraknipp@gmail.com>
2024-07-25 14:58:45 -0400

    Add an error message (to json route) if user forgets inputs.

--- routes/json.js
+++ routes/json.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const {parseParams, map} = require("./utils");
 
+router.get('', (req, res) => {
+  res.status(500);
+  res.json({error: "You neglected to include inputs in the url."});
+}
+
 router.get('/:rNmaxNmin', (req, res) => {
   let params = req.params.rNmaxNmin.split("-");
   let [error, xs] = parseParams(params);
