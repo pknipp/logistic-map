341ac48c5862a29b3b49409dd59448538e539dde
Peter Knipp <peteraknipp@gmail.com>
2024-07-26 11:02:29 -0400

    Unwrap italics tags from "r".

--- routes/svg.js
+++ routes/svg.js
@@ -26,7 +26,7 @@ router.get('/:rNmaxNmin', (req, res) => {
           text-anchor="middle"
           dy="0.32em"
         >
-          dynamics for growth-rate parameter <I>r</I> = ${rFactor}
+          dynamics for growth-rate parameter r = ${rFactor}
         </text>
       </g>
     `;
