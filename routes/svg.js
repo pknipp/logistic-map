773bafde2b89e8d87e1d6aaf57ce20e9491a0d1e
Peter Knipp <peteraknipp@gmail.com>
2024-07-29 17:36:04 -0400

    setAttribute to a string, not a boolean.

--- routes/svg.js
+++ routes/svg.js
@@ -177,7 +177,7 @@ router.get('/:rNmaxNmin', (req, res) => {
       <script>
         const toggleVisibility = path => {
           let visible = path.getAttribute("visiblity") === "visible";
-          path.setAttribute("visibility", !visible);
+          path.setAttribute("visibility", visible ? "visible" : "hidden");
         };
         let path = document.getElementByTagName("path)[0];
         let button = document.getElementByTagName("button")[0];
