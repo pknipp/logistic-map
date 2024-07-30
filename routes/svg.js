9b5505e39d48aa580c63235587aeabdb1928369c
Peter Knipp <peteraknipp@gmail.com>
2024-07-30 06:44:25 -0400

    Double line width.

--- routes/svg.js
+++ routes/svg.js
@@ -126,7 +126,7 @@ router.get('/:rNmaxNmin', (req, res) => {
         stroke="black"
         fill="transparent"
         visibility="hidden"
-        stroke-width="0.1"
+        stroke-width="0.2"
       />
     `;
     svg.el = `${svg.el}<g>${points}</g>${path}`;
