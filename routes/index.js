645ea12c5dbb290a2cfcdf41dfd5c9a89a1e6035
Peter Knipp <peteraknipp@gmail.com>
2025-04-08 14:32:06 -0400

    Insert button for example.

--- routes/index.js
+++ routes/index.js
@@ -70,8 +70,14 @@ router.get("", (req, res) => {
     let instructions = "<p><b>Instructions:</b> After <tt>...herokuapp.com</tt> above you should type either <tt>/api/</tt> or <tt>/</tt> depending upon whether you want the results returned as json or (graphical) html respectively. After that you should type the following two to four numerical inputs separated by hyphens: <ol><li> growth-rate parameter <i>r</I></li><li> number of generations to be calculated</li><li> (optional) initial value of <i>x</i> (If omitted this will be generated randomly.)</li><li> (optional) first generation to include in the results (If omitted this will include all generations.)</li></ol>";
     let html = top + title + back + background + instructions + `
         <div>
-            For instance the url https://logistic-map-ed5bb7ec94fe.herokuapp.com/3.54-100-0.42 should generate a graphic of an unstable two-cycle which evolves to a stable four-cycle after about 25 generations.
-        </div></div></body>`;
+            <b>Example:</b> <button>Click here</button> to see the url and the result for an unstable two-cycle which evolves to a stable four-cycle after about 25 generations.
+        </div></div>
+        <script>
+            const url = 'https://logistic-map-ed5bb7ec94fe.herokuapp.com/3.54-100-0.42';
+            const button = document.getElementsByTagName("button")[0];
+            button.addEventListener("click", () => window.location.href = url);
+        </script>
+        </body>`;
     res.send(html);
 });
 // ["api", ""].forEach(route => router.use(`/${route}`, require(`./${route}`)));
